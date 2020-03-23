8e5647f02a117686023b92267267ff31cc9c08b2
Robinson Castillo <42486305+Robindroide@users.noreply.github.com>
2020-03-22 21:23:36 -0600

    Update index.js

--- apiunahambre/apiunahambre/routes/index.js
+++ apiunahambre/apiunahambre/routes/index.js
@@ -30,7 +30,7 @@ const multer = require('multer');//Modulo para gestion de imagenes
 const uuid = require('uuid/v4');//Modulo para gestion de id de imagenes
 const path = require('path');
 const storage = multer.diskStorage({
-  destination: path.join(__dirname, 'public/uploadsProfilePics'),
+  destination: path.join(__dirname, '../public/uploadsProfilePics'),
   filename: (req, file, cb) => {
       cb(null, uuid() + path.extname(file.originalname).toLocaleLowerCase());
   }
